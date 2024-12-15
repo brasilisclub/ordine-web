62ece90de115572d29e0ca33e15fe669d839aa85
Thiago Campos <commit@thigcampos.com>
2024-12-15 14:57:08 -0300

    add a header in product listing page

--- src/app/(services)/produtos/page.tsx
+++ src/app/(services)/produtos/page.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
 import styles from "./produtos.module.css";
 
 export default function Produtos() {
+  
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>Produtos</h1>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Produtos</h1>
+        <Link href="" className={styles.link}>Adicionar</Link>
+      </header>
+      <section className={styles.listing}>
+        
+      </section>
     </main>
   );
 }
